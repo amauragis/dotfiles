f797c9d505ded04032e6515b2745a1de171cd9e1
Andy Mauragis <andrew@mauragis.com>
2023-08-04 08:52:33 -0400

    history search function

--- fish.config/log_shell_history.fish
+++ fish.config/log_shell_history.fish
@@ -13,3 +13,13 @@ end
 function log_cmd_history --on-event fish_preexec
     echo $(date "+%Y-%m-%dT%H:%M:%S") $(pwd) "\$" $argv[1] >> "$shell_history_dir/fish_"$(date "+%Y-%m-%d")".log"
 end
+
+if command -v rg &> /dev/null
+    function hist
+        rg -i $argv $shell_history_dir/*.log
+    end
+else
+    function hist
+        grep -i $argv $shell_history_dir/*.log
+    end
+end
